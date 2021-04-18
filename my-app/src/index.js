5f388b571cb0b0272888fc06dadb5096a1b9f8ee
Farzan Faisal <farzanalif42@gmail.com>
2021-04-17 23:07:12 -0400

    Update index.js
    Basic functionality implemented. Application generates random colour at refresh.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,15 +6,25 @@ class MainPage extends React.Component{
   constructor(props){
     super(props);
   }
-
   render(){
+    const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+    let r = random(0, 256);
+    let g = random(0, 256);
+    let b = random(0, 256);
+    let pageColor = "rgb(" + r.toString() + ", " + g.toString() + ", " + b.toString() + ")";
+    let alphaDiff = 60;
+    let titleColor = "rgb(" + (r - alphaDiff).toString() + ", " + (g - alphaDiff).toString() + ", " + (b - alphaDiff).toString() + ")";
+
+    document.documentElement.style.setProperty('--page-color', pageColor);
+    document.documentElement.style.setProperty('--title-color', titleColor);
     return(
-    <div>
-      <div className="ui-container">
-        <p>Random Color Generator</p>
+      <div>
+        <div className="ui-container">
+          <p>Random Color Generator</p>
+        </div>
       </div>
-    </div>
-      );
+    );
   }
 }
 
