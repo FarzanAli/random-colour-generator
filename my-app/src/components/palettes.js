b9ea4da491bbd75aabd426c68b8450d91279adda
Farzan Faisal <farzanalif42@gmail.com>
2021-04-21 23:08:34 -0400

    Components
    Redistributed application into components.

---
+++ my-app/src/components/palettes.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+export default class Palettes extends Component{
+    render(){
+        return(
+            <>
+            <div className="palette-container">
+                <div className="palette1"></div>
+                <div className="palette2"></div>
+                <div className="palette3"></div>
+                <div className="palette4"></div>
+            </div>
+            </>
+        );
+    }
+}
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Header from './components/header';
+import Palettes from './components/palettes';
 
 class MainPage extends React.Component{
-  
+
   constructor(props){
     super(props);
     this.state = {
@@ -17,7 +19,7 @@ class MainPage extends React.Component{
         this.startup();
       }
     });
-    
+
     this.startup();
   }
 
@@ -27,55 +29,47 @@ class MainPage extends React.Component{
     let g;
     let b;
 
-      const alphaDiff = [0, 20, 40, 50, 60, 80];
+    const alphaDiff = [0, 20, 40, 50, 60, 80];
 
-      const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-      const createColor = (r, g, b, alphaDiff) => "rgb(" + (r - alphaDiff).toString() + ", " + 
-                                                          (g - alphaDiff).toString() + ", " + 
-                                                          (b - alphaDiff).toString() + ")";
+    const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+    const createColor = (r, g, b, alphaDiff) => "rgb(" + (r - alphaDiff).toString() + ", " + 
+                                                        (g - alphaDiff).toString() + ", " + 
+                                                        (b - alphaDiff).toString() + ")";
 
-      function setColor(variable, color){
-        document.documentElement.style.setProperty(variable, color);
-      }
+    function setColor(variable, color){
+      document.documentElement.style.setProperty(variable, color);
+    }
 
-      r = random(0, 256);
-      g = random(0, 256);
-      b = random(0, 256);      
+    r = random(0, 256);
+    g = random(0, 256);
+    b = random(0, 256);      
 
-      let pageColor = createColor(r, g, b, alphaDiff[0]);
-      let titleColor = createColor(r, g, b, alphaDiff[4]);
-      let paletteColor1 = createColor(r, g, b, alphaDiff[1]);
-      let paletteColor2 = createColor(r, g, b, alphaDiff[2]);
-      let paletteColor3 = createColor(r, g, b, alphaDiff[3]);
-      let paletteColor4 = titleColor;
+    let pageColor = createColor(r, g, b, alphaDiff[0]);
+    let titleColor = createColor(r, g, b, alphaDiff[4]);
+    let paletteColor1 = createColor(r, g, b, alphaDiff[1]);
+    let paletteColor2 = createColor(r, g, b, alphaDiff[2]);
+    let paletteColor3 = createColor(r, g, b, alphaDiff[3]);
+    let paletteColor4 = titleColor;
 
-      setColor("--page-color", pageColor);
-      setColor("--text-color", titleColor);
-      setColor("--palette-color-1", paletteColor1);
-      setColor("--palette-color-2", paletteColor2);
-      setColor("--palette-color-3", paletteColor3);
-      setColor("--palette-color-4", paletteColor4);
-      this.setState({rgb: [r, g, b]}, ()=>console.log(this.state));
+    setColor("--page-color", pageColor);
+    setColor("--text-color", titleColor);
+    setColor("--palette-color-1", paletteColor1);
+    setColor("--palette-color-2", paletteColor2);
+    setColor("--palette-color-3", paletteColor3);
+    setColor("--palette-color-4", paletteColor4);
+    this.setState({rgb: [r, g, b]});
   }
 
   render(){
     return(
       <div>
-        <div className="ui-container">
-          <p>Random Colour Generator</p>
-        </div>
-        
-        <div className="palette-container">
-          <div className="palette1"></div>
-          <div className="palette2"></div>
-          <div className="palette3"></div>
-          <div className="palette4"></div>
-
-        </div>
-
+        <>
+        <Header/>
+        <Palettes/>
         <div className="description-container">
           RGB: ({this.state.rgb[0]}, {this.state.rgb[1]}, {this.state.rgb[2]})
         </div>
+        </>
       </div>
     );
   }
