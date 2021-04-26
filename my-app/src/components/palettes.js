4afc614e500e74d06eb572bca6bce244672b7392
Farzan Faisal <farzanalif42@gmail.com>
2021-04-26 02:33:47 -0400

    Update
    Added page color palette.

--- my-app/src/components/palettes.js
+++ my-app/src/components/palettes.js
@@ -14,10 +14,11 @@ export default class Palettes extends Component{
         return(
             <>
             <div className="palette-container">
+                <div className="palette" onMouseEnter={() => this.props.callbackHover(0)} onMouseLeave={() => this.props.callbackHover(0)} onClick={() => {this.copyToClipboardCallback(this.props.rgb[0], this.props.hex[0]); this.props.callbackClick();}}></div>
                 <div className="palette1" onMouseEnter={() => this.props.callbackHover(1)} onMouseLeave={() => this.props.callbackHover(0)} onClick={() => {this.copyToClipboardCallback(this.props.rgb[1], this.props.hex[1]); this.props.callbackClick();}}></div>
-                <div className="palette2" onMouseEnter={() => this.props.callbackHover(2)} onMouseLeave={() => this.props.callbackHover(0)} onClick={() => {this.copyToClipboardCallback(this.props.rgb[1], this.props.hex[1]); this.props.callbackClick();}}></div>
-                <div className="palette3" onMouseEnter={() => this.props.callbackHover(3)} onMouseLeave={() => this.props.callbackHover(0)} onClick={() => {this.copyToClipboardCallback(this.props.rgb[1], this.props.hex[1]); this.props.callbackClick();}}></div>
-                <div className="palette4" onMouseEnter={() => this.props.callbackHover(4)} onMouseLeave={() => this.props.callbackHover(0)} onClick={() => {this.copyToClipboardCallback(this.props.rgb[1], this.props.hex[1]); this.props.callbackClick();}}></div>
+                <div className="palette2" onMouseEnter={() => this.props.callbackHover(2)} onMouseLeave={() => this.props.callbackHover(0)} onClick={() => {this.copyToClipboardCallback(this.props.rgb[2], this.props.hex[2]); this.props.callbackClick();}}></div>
+                <div className="palette3" onMouseEnter={() => this.props.callbackHover(3)} onMouseLeave={() => this.props.callbackHover(0)} onClick={() => {this.copyToClipboardCallback(this.props.rgb[3], this.props.hex[3]); this.props.callbackClick();}}></div>
+                <div className="palette4" onMouseEnter={() => this.props.callbackHover(4)} onMouseLeave={() => this.props.callbackHover(0)} onClick={() => {this.copyToClipboardCallback(this.props.rgb[4], this.props.hex[4]); this.props.callbackClick();}}></div>
             </div>
             </>
         );
